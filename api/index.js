60c37c0011d047fdde5311ec6d66ac764a11f038
KidCharles <sircharlyrivera@gmail.com>
2025-07-20 15:45:19 -0400

    small changes to files

--- api/index.js
+++ api/index.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 const testDbRouter = require("./test-db");
 const polls = require("./polls");
-const app = express();
 
 router.use("/polls", polls);
 router.use("/test-db", testDbRouter);
-app.use(express.json());
-
 
 
 module.exports = router;
