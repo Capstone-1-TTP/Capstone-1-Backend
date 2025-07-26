d4452ccde176af28573fd4cc7916139f46ba366a
MrRobot458 <erivas157@gmail.com>
2025-07-26 17:12:08 -0400

    Cleaned up some code in the api folder

--- api/polls.js
+++ api/polls.js
@@ -12,23 +12,22 @@ router.get("/", async (req, res) => {
     console.error("Failed to fetch poll data", error);
   }
 });
+
 router.get("/mypolls/:userId", async (req, res) => {
-    const userId = Number(req.params.userId); 
-    try {
-        console.log("Fetching user's polls");
-        const polls = await Poll.findAll({
-        where: { userId: userId },
-        });
-        console.log(`Found ${polls} polls for user ${req.params.userId}`);
-        if (polls.length > 0) {
-        res.status(200).json(polls);
-        } else {
-        res.status(404).json({ error: "No polls found for this user" });
-        }
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch user's polls" });
+  const userId = Number(req.params.userId); 
+  try {
+    console.log("Fetching user's polls");
+    const polls = await Poll.findAll({ where: { userId: userId } });
+    console.log(`Found ${polls} polls for user ${req.params.userId}`);
+    if (polls.length > 0) {
+      res.status(200).json(polls);
+    } else {
+      res.status(404).json({ error: "No polls found for this user" });
     }
-    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch user's polls" });
+  }
+});
 
 router.get("/:id", async (req, res) => {
   try {
@@ -53,18 +52,17 @@ router.post("/", async (req, res) => {
 });
 
 router.delete("/:id", async (req, res) =>{
-    try{
-        const polls = await Poll.findByPk(req.params.id);
-        if(!polls){
-            return res.status(404).json({error: "Poll not found"});
-        }
-        await polls.destroy();
-        res.sendStatus(200);
-    }catch (error) {
-
-        console.error(error);
-        res.status(500).json({ error: "Failed to delete user" });
+  try {
+    const polls = await Poll.findByPk(req.params.id);
+    if (!polls) {
+      return res.status(404).json({error: "Poll not found"});
     }
+    await polls.destroy();
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to delete user" });
+  }
 });
 
 router.put("/:id", async (req, res) => {
@@ -73,45 +71,40 @@ router.put("/:id", async (req, res) => {
     if (!poll) {
       return res.status(404).json({ error: "Poll not found" });
     }
-
     const {title, description} = req.body;
-
     await poll.update({
-        title,
-        description,
+      title,
+      description,
     }); 
-
     res.json({
       message: `Poll titled ${poll.title} has been updated.`,
       Poll: poll,
     });
-}catch(error){
+  } catch (error) {
     console.error("PUT error occurred:", error);
     res.status(500).json({ error: "Failed to update polls" });
   }
 });
 
 router.patch("/:id", async (req, res) => {
-    try{
-        const poll = await Poll.findByPk(req.params.id);
-        if(!poll){
-            return res.status(404).json({error: "Poll not found"});
-        }
-        const {title, description} = req.body;
-        await poll.update({
-            title,
-            description,
-        });
-        res.json({
-            message: `Poll titled "${poll.title}" has been updated.`,
-            Poll: poll,
-        });
-    } catch(error){
-        console.error("PATCH error occurred:", error);
-        res.status(500).json({ error: "Failed to update poll..." });
+  try{
+    const poll = await Poll.findByPk(req.params.id);
+    if (!poll) {
+      return res.status(404).json({error: "Poll not found"});
     }
+    const {title, description} = req.body;
+    await poll.update({
+      title,
+      description,
+    });
+    res.json({
+      message: `Poll titled "${poll.title}" has been updated.`,
+      Poll: poll,
+    });
+  } catch(error) {
+    console.error("PATCH error occurred:", error);
+    res.status(500).json({ error: "Failed to update poll..." });
+  }
 });
-
-    
 
 module.exports = router;
